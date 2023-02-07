dbe342c53053462555e33f192aaa3c3519e1039b
chaudhuree <chaudhuree00@gmail.com>
2023-02-08 05:06:57 +0600

    fix some minor bug in dynamicallySetToken file  and update readme with postman file

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -78,7 +78,7 @@ const deleteJob = async (req, res) => {
   if (!job) {
     throw new NotFoundError(`No job with id ${jobId}`)
   }
-  res.status(StatusCodes.OK).send()
+  res.status(StatusCodes.OK).send("job deleted successfully")
 }
 
 module.exports = {
